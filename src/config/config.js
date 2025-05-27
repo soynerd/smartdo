003e24956c0aba3e33f012d7b9328b4d39fd42ec
Naveen Kumar <soynerd@soynerd.soynerd>
2025-05-27 21:41:24 +0530

    local_storage updated

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,8 @@ const auth={
     },
     local_Storage:{
         currentStorageKey: String(import.meta.env.VITE_LOCAL_STORAGE_AI_PROMPT_KEY),
-        newTaskStorageKey: String(import.meta.env.VITE_LOCAL_STORAGE_NEW_TASK_KEY)
+        newTaskStorageKey: String(import.meta.env.VITE_LOCAL_STORAGE_NEW_TASK_KEY),
+        userTasksStorageKey: String(import.meta.env.VITE_LOCAL_STORAGE_USER_TASKS_KEY)
     }
 }
 export default auth;
