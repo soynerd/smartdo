68645455dbe46fda19021bee0f7d3b6ff7c119b5
Naveen Kumar <soynerd@soynerd.soynerd>
2025-05-29 00:56:08 +0530

    fixed not loading task after saving

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,10 @@ const LoginPage = () => {
   const handleGoogleLogin = () => {
     window.location.href = "http://localhost:3000/auth/google";
   };
+  const handleGitHubLogin = () => {
+    window.location.href = "http://localhost:3000/auth/github";
+  };
+
   return (
     
     <div className="min-h-screen flex items-center justify-center  dark:from-gray-800 dark:to-gray-900 pb-60">
@@ -39,7 +43,10 @@ const LoginPage = () => {
           </div>
           {/* GitHub */}
           <div>
-            <button className="w-full min-w-[150px] flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-900 dark:focus:ring-indigo-500">
+            <button 
+            className="w-full min-w-[150px] flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-900 dark:focus:ring-indigo-500"
+            onClick={handleGitHubLogin}
+            >            
               <FaGithub className="h-5 w-5 mr-2 text-gray-800 dark:text-white" />
               GitHub
             </button>
