e41ec288a6cfddfbe5fa59a8f78dc92b9f05785e
soynerd <mesoynerd@gmail.com>
2025-07-23 17:56:29 +0530

    Better Error Handling

--- src/api/saveTask.js
+++ src/api/saveTask.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import auth from "../config/config";
 export default async function saveTask(tasks) {
-    try {        
-        await axios.post(auth.backend.api_url + "/data/updateTask", 
-          {taskData : tasks},
-          {withCredentials: true,});
+  try {
+    const res = await axios.post(
+      auth.backend.api_url + "/data/updateTask",
+      { taskData: tasks },
+      { withCredentials: true }
+    );
 
-          return true;
-      }catch(err){
-        console.log("api :: saveTask :: db :: ", err);
-        return false;
-      }
+    return { status: true, message: res.data.message, statusCode: res.status };
+  } catch (err) {
+    console.log("api :: saveTask :: db :: ", err);
+    return false;
+  }
 }
